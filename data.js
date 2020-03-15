295001ef21d57b2823504cca26da2570c05d44f9
Max Pahl <42980200+gitcaligo@users.noreply.github.com>
2020-03-15 22:28:47 +0100

    added answer dimensions, implemented function to calcuate result

--- data.js
+++ data.js
@@ -2,26 +2,96 @@ const quizdata = {
 
     item1 : {
         q : "question text example number 1",
-        a1 : "Yes1",
-        a2 : "No1"
+        a1 : "Yes",
+        a1_dim : "e",
+        a2 : "No",
+        a2_dim : "i"
     },
 
     item2 : {
         q : "question text example number 2",
-        a1 : "Yes2",
-        a2 : "No2"
+        a1 : "Yes",
+        a1_dim : "n",
+        a2 : "No",
+        a2_dim : "s"
     },
 
     item3 : {
         q : "question text example number 3",
-        a1 : "Yes3",
-        a2 : "No3"
+        a1 : "Yes",
+        a1_dim : "t",
+        a2 : "No",
+        a2_dim : "f"
     },
 
     item4 : {
         q : "question text example number 4",
-        a1 : "Yes4",
-        a2 : "No4"
+        a1 : "Yes",
+        a1_dim : "p",
+        a2 : "No",
+        a2_dim : "j"
+    },
+    item5 : {
+        q : "question text example number 5",
+        a1 : "Yes",
+        a1_dim : "e",
+        a2 : "No",
+        a2_dim : "i"
+    },
+
+    item6 : {
+        q : "question text example number 6",
+        a1 : "Yes",
+        a1_dim : "n",
+        a2 : "No",
+        a2_dim : "s"
+    },
+
+    item7 : {
+        q : "question text example number 7",
+        a1 : "Yes",
+        a1_dim : "t",
+        a2 : "No",
+        a2_dim : "f"
+    },
+
+    item8 : {
+        q : "question text example number 8",
+        a1 : "Yes",
+        a1_dim : "p",
+        a2 : "No",
+        a2_dim : "j"
+    },
+    item9 : {
+        q : "question text example number 9",
+        a1 : "Yes",
+        a1_dim : "e",
+        a2 : "No",
+        a2_dim : "i"
+    },
+
+    item10 : {
+        q : "question text example number 10",
+        a1 : "Yes",
+        a1_dim : "n",
+        a2 : "No",
+        a2_dim : "s"
+    },
+
+    item11 : {
+        q : "question text example number 11",
+        a1 : "Yes",
+        a1_dim : "t",
+        a2 : "No",
+        a2_dim : "f"
+    },
+
+    item12 : {
+        q : "question text example number 12",
+        a1 : "Yes",
+        a1_dim : "p",
+        a2 : "No",
+        a2_dim : "j"
     }
 
 };
--- main.js
+++ main.js
@@ -3,8 +3,6 @@ let results = [];
 
 let pointer = results.length + 1;
 
-//console.log(pointer);
-
 function writeAll() {
     $('#questions h2').html('Frage ' + pointer);
     $('#questions p').html(quizdata["item" + pointer].q);
@@ -13,7 +11,34 @@ function writeAll() {
 };
 
 function showResult() {
-    $('#result p').html(results);
+    // map results
+    let map = results.reduce(function(prev, cur) {
+        prev[cur] = (prev[cur] || 0) + 1;
+        return prev;
+    }, {});
+
+    // set dominant trait
+    map.e > map.i || map.i == undefined ? dim1Name = 'Extraversion' : dim1Name = 'Introversion';
+    map.n > map.s || map.s == undefined ? dim2Name = 'Intuition' : dim2Name = 'Sensing';
+    map.t > map.f || map.f == undefined ? dim3Name = 'Thinking' : dim3Name = 'Feeling';
+    map.p > map.j || map.j == undefined ? dim4Name = 'Probing' : dim4Name = 'Judging';
+
+    // count dominant trait answers
+    map.e > map.i || map.i == undefined ? dim1Count = map.e : dim1Count = map.i;
+    map.n > map.s || map.s == undefined ? dim2Count = map.n : dim2Count = map.s;
+    map.t > map.f || map.f == undefined ? dim3Count = map.t : dim3Count = map.f;
+    map.p > map.j || map.j == undefined ? dim4Count = map.p : dim4Count = map.j;
+
+    // calculate percent 
+    map.e == undefined || map.i == undefined ? dim1Percent = 100 : dim1Percent = (dim1Count/(map.e + map.i)*100).toFixed(1);
+    map.n == undefined || map.s == undefined ? dim2Percent = 100 : dim2Percent = (dim2Count/(map.n + map.s)*100).toFixed(1);
+    map.t == undefined || map.f == undefined ? dim3Percent = 100 : dim3Percent = (dim3Count/(map.t + map.f)*100).toFixed(1);
+    map.p == undefined || map.j == undefined ? dim4Percent = 100 : dim4Percent = (dim4Count/(map.p + map.j)*100).toFixed(1);
+
+    $('#dim1').html(dim1Name + ' : ' + dim1Count + ' (' + dim1Percent + '%)');
+    $('#dim2').html(dim2Name + ' : ' + dim2Count + ' (' + dim2Percent + '%)');
+    $('#dim3').html(dim3Name + ' : ' + dim3Count + ' (' + dim3Percent + '%)');
+    $('#dim4').html(dim4Name + ' : ' + dim4Count + ' (' + dim4Percent + '%)');
 };
 
 $(document).ready( function() {
@@ -23,13 +48,13 @@ $(document).ready( function() {
 
     $('#a1').click( function() {
         if (pointer < Object.keys(quizdata).length || results.length + 1 == Object.keys(quizdata).length) {
-            results.push("a1")
+            results.push(quizdata["item" + pointer].a1_dim)
         }
     });
 
     $('#a2').click( function() {
         if (pointer < Object.keys(quizdata).length || results.length + 1 == Object.keys(quizdata).length) {   
-            results.push("a2");
+            results.push(quizdata["item" + pointer].a2_dim);
         }
     });
 
@@ -48,8 +73,6 @@ $(document).ready( function() {
         if (pointer == 2) {
             $('#back').toggle();
         };
-        console.log(pointer);
-        console.log(results);
     });
 
     $('#back').click( function() {
@@ -61,8 +84,6 @@ $(document).ready( function() {
             writeAll();
             results.pop();
         };
-        console.log(pointer);
-        console.log(results);
     });
 
     $('#reset').click( function() {
@@ -72,8 +93,5 @@ $(document).ready( function() {
         $('#questions').toggle();
         $('#result').toggle();
         $('#back').toggle();
-
-        console.log(pointer);
-        console.log(results);
     });
 });
